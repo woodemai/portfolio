4945780300a38fd0d3201f70cca0c29689c3db14
woodemai <n.savchenkoo73@gmail.com>
2023-11-28 20:32:08 +0300

    fix bugs

--- src/app/providers/PageProvider/router.tsx
+++ src/app/providers/PageProvider/router.tsx
@@ -4,28 +4,42 @@ import HomePage from "@/pages/home/HomePage";
 import WarehousePage from "@/pages/projects/warehouse";
 import EducationPage from "@/pages/projects/education";
 import GaragePage from "@/pages/projects/garageSale";
+import { createRef } from "react";
 
+export const routes = [
+    {
+        path: "/",
+        name: "Home",
+        element: <HomePage />,
+        nodeRef: createRef()
+    },
+    {
+        path: '/warehouse',
+        name: "Warehouse",
+        element: <WarehousePage />,
+        nodeRef: createRef()
+    },
+    {
+        path: '/new-education',
+        name: "New education",
+        element: <EducationPage />,
+        nodeRef: createRef()
+    },
+    {
+        path: '/garage-sale',
+        name: "Garage sale",
+        element: <GaragePage />,
+        nodeRef: createRef()
+    },
+]
 const router = createBrowserRouter([
     {
         element: <RootLayout />,
-        children: [
-            {
-                path: "/",
-                element: <HomePage />,
-            },
-            {
-                path: '/warehouse',
-                element: <WarehousePage/>
-            },
-            {
-                path: '/new-education',
-                element: <EducationPage/>
-            },
-            {
-                path: '/garage-sale',
-                element: <GaragePage/>
-            },
-        ]
+        children: routes.map(route => ({
+            index: route.path === '/',
+            path: route.path === '/' ? undefined : route.path,
+            element: route.element
+        }))
     }
 ])
 
