d6e38d2806086e60eba1995fbbc29eb5099dfbf5
woodemai <n.savchenkoo73@gmail.com>
2023-11-28 17:14:14 +0300

    add simple animation

--- src/app/providers/PageProvider/RootLayout.tsx
+++ src/app/providers/PageProvider/RootLayout.tsx
@@ -1,12 +1,34 @@
+import { CSSTransition, SwitchTransition } from 'react-transition-group'
 import { Toaster } from "@/shared/components/ui/toaster";
 import { Outlet } from "react-router";
 import Header from "./Header";
+import { useLocation, useOutlet } from 'react-router-dom';
+import router from './router';
 
 export default function RootLayout() {
+    const location = useLocation()
+    const currentOutlet = useOutlet()
+    
+  const { nodeRef } = router.routes.find((route) => route.path === location.pathname) ?? {}
     return (
         <>
             <Header/>
             <Toaster/>  
+            <SwitchTransition>
+                <CSSTransition
+                    key={location.pathname}
+                    nodeRef={nodeRef}
+                    timeout={300}
+                    classNames={'page'}
+                    unmountOnExit
+                >
+                    {() => (
+                        <div ref={nodeRef}>
+                            {currentOutlet}
+                        </div>
+                    )}
+                </CSSTransition>
+            </SwitchTransition>
             <Outlet />
         </>
     )
